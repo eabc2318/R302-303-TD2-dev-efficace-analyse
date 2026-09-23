2b6112aac37116446a8737dfc178dc52a1393a96
eabc2318 <mathias.m2502@gmail.com>
2026-09-23 15:27:00 +0200

    finition de synglist

--- Singlylist.java
+++ Singlylist.java
@@ -1,35 +1,82 @@
-package singlyListPkg;
-
-public class SinglyListClass {
-    private Node header;
-
-    public SinglyListClass(Node header) {
-        this.header = header;
-    }
-
-    public void addLast(Integer element) {
-        Node newNode = new Node(element);
-        Node tmpHeader = header;
-        if (header == null) {
-            header = newNode;
-            return;
-        }
-        if (tmpHeader.getNext() == null) { // si tmpHeader est null alors on est sur le dernier element
-            // alors on positionne tmpHeader
-            tmpHeader.setNext(newNode);
-        }
-    }
-
+public class SinglyList {
     private static class Node {
         private Integer element;
         private Node next;
+        public Node(Integer e, Node n) { element = e; next = n; }
+        public Integer getElement() { return element; }
+        public Node getNext() { return next; }
+        public void setNext(Node n) { next = n; }
+    }
 
-        public Node getNext() {
-            return next;
-        }
+    private Node head = null;
+    private Node tail = null;
+    private int size = 0;
 
-        public void setNext(Node next) {
-            this.next = next;
+    public SinglyList() {}
+
+    
+    public int size() { return size; }
+    public boolean isEmpty() { return size == 0; }
+    
+    public Integer first() {
+        if (isEmpty()) return null;
+        return head.getElement();
+    }
+    
+    public Integer last() {
+        if (isEmpty()) return null;
+        return tail.getElement();
+    }
+    
+    public void addFirst(Integer e) {
+        head = new Node(e, head);
+        if (size == 0) tail = head;
+        size++;
+    }
+    
+    public void addLast(Integer e) {
+        Node newest = new Node(e, null);
+        if (isEmpty()) head = newest;
+        else tail.setNext(newest);
+        tail = newest;
+        size++;
+    }
+    
+    public Integer removeFirst() {
+        if (isEmpty()) return null;
+        Integer answer = head.getElement();
+        head = head.getNext();
+        size--;
+        if (size == 0) tail = null;
+        return answer;
+    }
+
+    
+    
+    
+    public Integer getSecondToLast() {
+        if (size < 2) return null;
+        Node current = head;
+        while (current.getNext().getNext() != null) {
+            current = current.getNext();
         }
+        return current.getElement();
+    }
+
+    
+    public void reverse() {
+        if (size <= 1) return;
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        tail = head; 
+        
+        while (current != null) {
+            next = current.getNext();
+            current.setNext(prev);
+            prev = current;
+            current = next;
+        }
+        head = prev; 
     }
 }
